28c8eb898cf7d6862c899752657c2994ab06ce19
Umer Syed-Quadri <syedquadri.u@northeastern.edu>
2025-04-08 16:35:16 -0400

    fix: added methods for the interface

--- src/main/java/student/model/IModel.java
+++ src/main/java/student/model/IModel.java
@@ -1,5 +1,40 @@
 package student.model;
 
+import java.io.OutputStream;
+
+import java.util.Collection;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import student.model.formatters.DataFormatter;
+import student.model.formatters.Formats;
+
+/**
+ * Interface to the model.
+ */
 public interface IModel {
-    
+    String DATABASE = "data/records.xml";
+
+    Collection<AnimalRecord> getRecords();
+
+    AnimalRecord getRecord(String name);
+
+    static void writeRecords(Collection<AnimalRecord> records, Formats format, OutputStream out) {
+        DataFormater.write(records, format, out);
+    }
+/*
+    static IModel getInstance() {
+        return getInstance(DATABASE);
+    }
+
+    static IModel getInstance(String database) {
+        return new AnimalJamModel(database);
+    }
+*/
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JacksonXmlRootElement(localName = "animal")
+    @JsonPropertyOrder({"name", "population_size", "speed", "averge_weight", "diet", "location"})
+    record AnimalRecord(String name, double population_size, double speed, double average_weight, 
+                    String diet, String location) {
+    }
 }
