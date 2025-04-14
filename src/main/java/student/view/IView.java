b58108b27f2f75418ebde65297b52860dcec8399
Kyenex <mrashvernon@gmail.com>
2025-04-14 17:12:46 -0400

    feat: added view interface methods

--- src/main/java/student/view/IView.java
+++ src/main/java/student/view/IView.java
@@ -1,5 +1,33 @@
 package student.view;
 
+import java.util.Collection;
+
 public interface IView {
     
+    /*
+     * This method is used to display a collection of items and its subsets(Including the favorite list, sorted and filtered lists)
+     * @param collection The collection of items to be displayed
+     */
+    void displayCollection(Collection collection);
+
+    /*
+     * This method is used to display a message to the user(e.g., error message, success message)
+     * @param item The item to be displayed
+     */
+    void displayMessage(String Message, boolean isError);
+
+    /*
+     * This method is used to display a show Filter JOptionPane
+     */
+    void displayFilterPopup();
+
+    /*
+     * This method is used to display a show Sort JOptionPane
+     */
+    void displaySortPopup();
+
+    /*
+     * This method is used to display a show Favorite JOptionPane
+     */
+    void displaySavePopup();
 }
