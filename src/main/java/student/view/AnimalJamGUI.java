cedea6f6bfaac8524c9c8cb8c5352aa8fdd3c74d
Kyenex <mrashvernon@gmail.com>
2025-04-14 22:38:01 -0400

    feat: added unimplemented GUI class

---
+++ src/main/java/student/view/AnimalJamGUI.java
@@ -0,0 +1,44 @@
+package student.view;
+
+import java.util.Collection;
+
+import student.view.IView;
+
+public class AnimalJamGUI implements IView{
+    
+    public AnimalJamGUI() {
+
+    }
+
+    @Override
+    public void displayCollection(Collection collection) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayCollection'");
+    }
+
+    @Override
+    public void displayMessage(String Message, boolean isError) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayMessage'");
+    }
+
+    @Override
+    public void displayFilterPopup() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayFilterPopup'");
+    }
+
+    @Override
+    public void displaySortPopup() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displaySortPopup'");
+    }
+
+    @Override
+    public void displaySavePopup() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displaySavePopup'");
+    }
+
+    
+}
