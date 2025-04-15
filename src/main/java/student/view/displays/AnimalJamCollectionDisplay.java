69ecaacb73b2db71fed52980124b536021609be7
Kyenex <mrashvernon@gmail.com>
2025-04-15 16:22:25 -0400

    feat: added favorite list button to collection display

--- src/main/java/student/view/displays/AnimalJamCollectionDisplay.java
+++ src/main/java/student/view/displays/AnimalJamCollectionDisplay.java
@@ -26,15 +26,21 @@ public class AnimalJamCollectionDisplay {
         JButton searchButton = new JButton("Search");
         searchButton.setBounds(850,30,80,25);
 
+        // Create Button to switch to Favorite List
+        JButton favoriteListButton = new JButton("Favorite List");
+        favoriteListButton.setBounds(60, 500, 200, 25);
+
         // Create JTable for the Collection 
         JTable colTable = new JTable(data, heading);
         JScrollPane scroll = new JScrollPane(colTable);
         scroll.setBounds(60,80,870,400);
 
+
         // Add elements to frame
         frame.add(scroll);
         frame.add(searchText);
         frame.add(searchButton);
+        frame.add(favoriteListButton);
         frame.setVisible(true);
     }
 
