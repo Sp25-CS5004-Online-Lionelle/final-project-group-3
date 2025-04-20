5f76865249981bba1af64ce8b97bf4d9eee050cc
Umer Syed-Quadri <syedquadri.u@northeastern.edu>
2025-04-20 01:39:20 -0400

    fix: added test for getCollection method

---
+++ src/test/java/TestAnimalJamController.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.swing.SortOrder;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import student.model.formatters.Formats;
+import student.controller.AnimalJamController;
+import student.controller.IAnimalController;
+import student.model.AnimalJamModel;
+import student.model.IAnimalModel;
+import student.model.IAnimalModel.AnimalRecord;
+
+public class TestAnimalJamController {
+    static IAnimalModel model;
+    static IAnimalController controller;
+    Collection<AnimalRecord> recordList;
+
+    @BeforeAll
+    public static void setup() {
+        String filename = "data/test_sample.csv";
+        model = IAnimalModel.getInstance(filename); 
+        controller = new AnimalJamController(model);
+    }
+
+    @Test
+    public void testGetColletion() {
+        //Get list of records
+        recordList = controller.getCollection();
+        assertEquals(16, recordList.size());
+    }
+
+    @Test
+    public void testAddToFavList() {
+        //Get list of records
+        recordList = controller.getCollection();
+        assertEquals(16, recordList.size());
+    }
+}
