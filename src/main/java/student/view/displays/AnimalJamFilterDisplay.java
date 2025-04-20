837b8f86c5db7c417b1288a5991db6b37a700fd6
Kyenex <mrashvernon@gmail.com>
2025-04-20 04:20:40 -0400

    feat: refactored `AnimalJamFilterDisplay` to get filter string, sort column and sort order.

--- src/main/java/student/view/displays/AnimalJamFilterDisplay.java
+++ src/main/java/student/view/displays/AnimalJamFilterDisplay.java
@@ -1,5 +1,8 @@
 package student.view.displays;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -16,40 +19,43 @@ public class AnimalJamFilterDisplay {
     
     JFrame frame;
     String[] options;
+    AnimalJamCollectionDisplay instance;
     IAnimalController controller;
 
-    public AnimalJamFilterDisplay(JFrame frame, String[] options) {
+    public AnimalJamFilterDisplay(AnimalJamCollectionDisplay instance, JFrame frame, String[] options, IAnimalController controller) {
 
         // Get passed in variables
         this.frame = frame;
         this.options = options;
+        this.instance = instance;
+        this.controller = controller;
 
-        // Create a JDIalog and set size and location
+        // Create a JDDialog and set size and location
         JDialog formatDialog = new JDialog(frame,"Filter", true);
         formatDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         formatDialog.setSize(400,370);
         formatDialog.setLocationRelativeTo(frame);
         formatDialog.setLayout(null);
         
-        // Create Text Label
-        JLabel filterOnLabel = new JLabel("Filter on: ");
-        filterOnLabel.setBounds(40, 30, 80, 25);
-
-        // Create JComboBox with columns to choose from
-        JComboBox<String> filterColumn = new JComboBox<>(options);
-        filterColumn.setBounds(40, 60, 300, 30);
-        
         // Create Text Label for filter
-        JLabel filterLabel = new JLabel("Filter: ");
-        filterLabel.setBounds(40, 110, 80, 25);
+        JLabel filterLabel = new JLabel("Enter Filters: ");
+        filterLabel.setBounds(40, 30, 80, 25);
 
         // Create JTextBox to input filter
         JTextField filter = new JTextField();
-        filter.setText("Enter Filter Here");
-        filter.setBounds(40, 140, 300, 30);
+        filter.setBounds(40, 60, 300, 30);
+
+        // Create Text Label
+        JLabel filterOnLabel = new JLabel("Sort on: ");
+        filterOnLabel.setBounds(40, 110, 80, 25);
+
+        // Create JComboBox with columns to choose from
+        JComboBox<String> filterColumn = new JComboBox<>(options);
+        filterColumn.setBounds(40, 140, 300, 30);
+        
 
         // Create label for the filter order
-        JLabel sortOrderLabel = new JLabel("Filter Order: ");
+        JLabel sortOrderLabel = new JLabel("Sort Order: ");
         sortOrderLabel.setBounds(40, 190, 80, 25);
 
         // Create radio buttons for ascending and descending order
@@ -58,6 +64,11 @@ public class AnimalJamFilterDisplay {
         JRadioButton descending = new JRadioButton("Descending", false);
         descending.setBounds(40, 260, 100, 25);
 
+        // Add radio buttons to a list
+        List<JRadioButton> radioButtons = new ArrayList<>();
+        radioButtons.add(ascending);
+        radioButtons.add(descending);
+        
         // Group radio buttons
         ButtonGroup group = new ButtonGroup();
         group.add(ascending);
@@ -66,7 +77,7 @@ public class AnimalJamFilterDisplay {
         // Create buttons to apply and cancel filters
         JButton apply = new JButton("Apply");
         apply.setBounds(150, 285, 100, 25);
-        apply.addActionListener(FilterAL.applyFilterButtonListener(formatDialog, filter, filterColumn));
+        apply.addActionListener(FilterAL.applyFilterButtonListener(instance, formatDialog, filter, filterColumn, radioButtons, controller));
 
         JButton cancel = new JButton("Cancel");
         cancel.setBounds(270, 285, 100, 25);
@@ -87,21 +98,21 @@ public class AnimalJamFilterDisplay {
         formatDialog.setVisible(true);
     }
 
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
         
-        JFrame frame = new JFrame("AnimalJam");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //     JFrame frame = new JFrame("AnimalJam");
+    //     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.setSize(800, 600);
+    //     frame.setSize(800, 600);
 
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+    //     frame.setLocationRelativeTo(null);
+    //     frame.setVisible(true);
 
-        String[] options = {"Name","Population","Speed","Average Weight","Diet","Continental Location"};
+    //     String[] options = {"Name","Population","Speed","Average Weight","Diet","Continental Location"};
 
-        AnimalJamFilterDisplay filter = new AnimalJamFilterDisplay(frame, options);
+    //     AnimalJamFilterDisplay filter = new AnimalJamFilterDisplay(frame, options);
 
-    }
+    // }
 }
 
 
