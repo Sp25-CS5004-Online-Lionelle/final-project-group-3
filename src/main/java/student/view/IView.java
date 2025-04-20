0d67282820e67e06255901b67d40508531949c56
Kyenex <mrashvernon@gmail.com>
2025-04-20 06:37:35 -0400

    feat: removed unnecessary methods from  `IVIew` interface

--- src/main/java/student/view/IView.java
+++ src/main/java/student/view/IView.java
@@ -1,36 +1,11 @@
 package student.view;
 
-import student.model.IAnimalModel.AnimalRecord;
-
-import java.util.Collection;
-
 public interface IView {
-    
-    /*
-     * This method is used to display a collection of items and its subsets(Including the favorite list, sorted and filtered lists)
-     * @param collection The collection of items to be displayed
-     */
-    void displayCollection(Collection<AnimalRecord> collection);
 
-    /*
-     * This method is used to display a message to the user(e.g., error message, success message)
-     * @param item The item to be displayed
+    /**
+     * THis method is used to run the program and display the GUI.
+     * It is called when the program is started.
      */
-    void displayMessage(String Message, boolean isError);
-
-    /*
-     * This method is used to display a show Filter JOptionPane
-     */
-    void displayFilterPopup();
-
-    /*
-     * This method is used to display a show Sort JOptionPane
-     */
-    void displaySortPopup();
-
-    /*
-     * This method is used to display a show Favorite JOptionPane
-     */
-    void displaySavePopup();
+    void run();
 
 }
