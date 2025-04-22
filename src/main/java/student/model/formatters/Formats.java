e65152d1234091fe5df051eb69777614be2d00b9
Umer Syed-Quadri <syedquadri.u@northeastern.edu>
2025-04-22 11:13:05 -0400

    fix: add option for TXT format

--- src/main/java/student/model/formatters/Formats.java
+++ src/main/java/student/model/formatters/Formats.java
@@ -3,7 +3,7 @@ package student.model.formatters;
 /** A list of format types allowed for export and import. */
 public enum Formats {
    /** Different formatting options. */
-   JSON, XML, CSV, PRETTY;
+   JSON, XML, CSV, PRETTY, TXT;
 
    /**
     * Helper function to check if a value is in the list of formats.
